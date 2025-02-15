2a755946cf7d98839fa476ba7f98053aafb79cc5
Emad Abaei <emad.ae14@gmail.com>
2025-02-15 15:50:29 +0330

    fix(StarRating): resolve Safari issue

--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 interface StarRatingProps {
   onRate: (rate: number) => void;
@@ -11,9 +11,9 @@ interface StarRatingProps {
 function StarRating({
   onRate,
   maxRating = 5,
-  color = "#FFD700",
+  color = '#FFD700',
   size = 25,
-  message = false
+  message = false,
 }: StarRatingProps) {
   const [tempRating, setTempRating] = useState(0);
   const [rating, setRating] = useState(0);
@@ -22,21 +22,22 @@ function StarRating({
   const stars = Array.from({ length: maxRating }, (_, i) => i);
 
   const containerStyle = {
-    display: "flex",
-    alignItems: "center"
+    display: 'flex',
+    alignItems: 'center',
   };
 
   const starStyle = {
-    display: 'inlineBlock',
-    maxWidth: `${size}px`,
+    display: 'inline-block',
+    width: `${size}px`,
+    height: `${size}px`,
     stroke: color,
-    cursor: "pointer",
+    cursor: 'pointer',
   };
 
   const messageStyle = {
     fontSize: `${size / 1.4}px`,
-    marginLeft: "10px",
-    color: color
+    marginLeft: '10px',
+    color: color,
   };
 
   function handleMouseEnter(tempRate: number): void {
@@ -57,39 +58,41 @@ function StarRating({
       {stars.map((_, ix) =>
         full < ix + 1 ? (
           <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            stroke={color ? color : "#FFD700"}
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke={color ? color : '#FFD700'}
             key={ix}
             style={starStyle}
             onMouseEnter={() => handleMouseEnter(ix + 1)}
             onMouseLeave={handleMouseLeave}
-            onClick={() => handleClick(ix + 1)}>
+            onClick={() => handleClick(ix + 1)}
+          >
             <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='{2}'
-              d='M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z'
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="{2}"
+              d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
             />
           </svg>
         ) : (
           <svg
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 20 20'
-            fill={color ? color : "#FFD700"}
-            stroke={color ? color : "#FFD700"}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill={color ? color : '#FFD700'}
+            stroke={color ? color : '#FFD700'}
             key={ix}
             style={starStyle}
             onMouseEnter={() => handleMouseEnter(ix + 1)}
             onMouseLeave={handleMouseLeave}
-            onClick={() => handleClick(ix + 1)}>
-            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+            onClick={() => handleClick(ix + 1)}
+          >
+            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
           </svg>
         )
       )}
       {message && (
-        <span style={messageStyle}>{tempRating || rating || ""}</span>
+        <span style={messageStyle}>{tempRating || rating || ''}</span>
       )}
     </div>
   );
